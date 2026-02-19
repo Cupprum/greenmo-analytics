4198215c8d044c323ac57363826ad399e1f57fc9
Cupprum <branisa.samuel@icloud.com>
2026-02-19 13:34:08 +0100

    chore: execute teh calculations

--- main.go
+++ main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"greenmo/analytics"
 	"greenmo/gql"
+	"greenmo/httpapi"
 	"greenmo/tools"
 )
 
 func main() {
-	// res, err := tools.FetchData("reservations.json", httpapi.Data)
-	// if err != nil {
-	// 	panic(fmt.Errorf("failed to fetch data: %w", err))
-	// }
+	res, err := tools.FetchData("reservations.json", httpapi.Data)
+	if err != nil {
+		panic(fmt.Errorf("failed to fetch data: %w", err))
+	}
 
-	// err = analytics.Calculate(res)
-	// if err != nil {
-	// 	panic(fmt.Errorf("failed to calculate interesting facts: %w", err))
-	// }
-
-	res, err := tools.FetchData("financials.json", gql.Data)
+	fins, err := tools.FetchData("financials.json", gql.Data)
 	if err != nil {
 		panic(fmt.Errorf("failed to fetch Gql data: %w", err))
 	}
-	fmt.Println(res)
+
+	err = analytics.Calculate(res, fins)
+	if err != nil {
+		panic(fmt.Errorf("failed to calculate interesting facts: %w", err))
+	}
 }
